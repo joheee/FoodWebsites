e3a3e182b2007c7f1ba0b8ebb133285f9a19e5d8
Johevin Blesstowi <BS22-1@slc.net>
2024-05-21 12:30:13 +0700

    dynamic parallax based on page

--- src/components/parallax/parallax.js
+++ src/components/parallax/parallax.js
@@ -5,15 +5,41 @@ link.rel = "stylesheet";
 link.href = url;
 document.head.appendChild(link);
 
+// logic
+const currentPath = window.location.pathname;
+const pathSplit = currentPath.split("/");
+const parallaxDic = {
+  home: {
+    image: "../../../assets/home-background.jpg",
+    title: "VKellog's",
+  },
+  food: {
+    image: "../../../assets/product-background.jpg",
+    title: "Foods",
+  },
+  recipe: {
+    image: "../../../assets/recipe-background.jpg",
+    title: "Recipes",
+  },
+  about: {
+    image: "../../../assets/about-background.jpg",
+    title: "About Us",
+  },
+  contact: {
+    image: "../../../assets/home-background.jpg",
+    title: "Contact Us",
+  },
+};
+
 // create element
 const element = `
 <div class="parallax-container">
-    <div class="parallax-title">VKellog's</div>
+    <div class="parallax-title">${parallaxDic[pathSplit[3]].title}</div>
     <div class="parallax-image-container">
     <div class="parallax-image-dark-layer"></div>
     <img
         class="parallax-image"
-        src="../../../assets/home-background.jpg"
+        src="${parallaxDic[pathSplit[3]].image}"
         alt=""
     />
     </div>
